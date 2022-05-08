46412a5e31df5269ddbaab6c15ba7b7f3a84a9cb
armgp <arm.gp23@gmail.com>
2022-05-08 10:38:42 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { Navbar, Favounav, Footer } from './components';
+import { Navbar, Favounav, Footer, Login, Signup } from './components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Favounav />
+      <Routes>
+        <Route path="/" element={<Favounav />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
         <header class="mx-24 my-5 border-2 border-slate-900 rounded-3xl">
             <div class="max-w-screen-xl p-4 mx-auto">
                 <div class="flex items-center justify-between space-x-4 lg:space-x-10">
-                    <div class="flex lg:w-0 lg:flex-1">
+                    <Link to={"/"} class="flex lg:w-0 lg:flex-1">
                         <span class="w-12 h-12 bg-gray-200 rounded-lg border-2 border-black"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAABmJLR0QA/wD/AP+gvaeTAAAAjElEQVRoge3XQQqAMAwAwSj+/1Pqu+rVi5hIrQvugCcFu1SKiZA+t0dEG3yt2cVNhZBWeLan1Brnt1cxiiE0htDQQ9LH79L5xZXjvCv6jqQZQmMIjSE0htAYcmHoeHtGHXXL/2x+WjSG0BBDHp1aTog0htAYQmMIjSE0htAYQmMIzS9Dtpv7jyY7Ce4AMGE0hs2u+BwAAAAASUVORK5CYII=" /></span>
-                    </div>
+                    </Link>
 
 
 
                     <div class="items-center justify-end flex-1 hidden space-x-4 sm:flex">
-                        <button
-                            class="border-2 border-black px-5 py-2 text-sm font-medium text-black bg-gray-100 rounded-lg"
+                        <Link to={"login"}
+                            class="border-2 border-black px-5 py-2 text-sm font-medium hover:text-white hover:bg-black text-black bg-gray-100 rounded-lg"
                             href=""
                         >
                             Log in
-                        </button>
+                        </Link>
 
-                        <button
-                            class="border-2 border-black px-5 py-2 text-sm font-medium text-white bg-black rounded-lg"
+                        <Link to={"signup"}
+                            class="border-2 border-black px-5 py-2 text-sm font-medium hover:text-black hover:bg-gray-100 text-white bg-black rounded-lg"
                             href=""
                         >
                             Sign up
-                        </button>
+                        </Link>
                     </div>
 
                     <div class="lg:hidden">
