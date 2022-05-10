fa4dec76c292ccad03f57c899152def33b20de8f
armgp <arm.gp23@gmail.com>
2022-05-10 14:41:08 +0530

    some css

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Favounav />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div class="flex flex-col h-screen justify-between">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Favounav />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   )
 }
