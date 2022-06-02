5cbe9cadfdb360efe10074c9c19b2a2c366f5e03
armgp <armgp@users.noreply.github.com>
2022-06-02 20:46:50 +0530

    favoun card name size change from xl to lg

--- src/components/Favoun.jsx
+++ src/components/Favoun.jsx
@@ -1,7 +1,7 @@
 const Favoun = (props) => {
     return (
-        <div class="flex h-64 w-56 flex-col items-center rounded-lg border-2 border-black hover:border-y-4">
-            <div class="py-6 text-xl font-extrabold">{props.name}</div>
+        <div class="flex h-64 w-56 px-1 flex-col items-center rounded-lg border-2 border-black hover:border-y-4">
+            <div class="py-6 text-lg font-extrabold">{props.name}</div>
             <div class="h-48 overflow-auto scrollbar-hide">
                 <div class="px-6 font-mono text-sm font-semibold">{props.description}</div>
             </div>
